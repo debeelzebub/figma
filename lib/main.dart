ca08748e8e2493ee09b065435478a9459f534c29
debeelzebub <sruthysurendrants@gamil.com>
2023-10-06 13:06:11 +0530

    new

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// ignore: unused_import
+// import 'dart:js';
+import 'package:project/screens/login.dart';
+// import 'dart:ui';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( const MyApp(), 
+  );
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // theme: ThemeData(
+       
+      //   primarySwatch: Colors.green,
+      // ),
+      home: My_Login(),
+    );
+  }
+}
+
